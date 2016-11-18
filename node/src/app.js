1082a501e4b149a5771e5e690864726aba074b2d
Deltax9k <deltax9000@163.com>
2016-11-18 21:31:59 +0800

    try to commit!

--- node/src/app.js
+++ node/src/app.js
@@ -1,22 +1,8 @@
-//可以采用以下这种方式引入静态资源, 除了css, 还包括字体, 图片等
-;[
-    './resources/app.css',
-    './resources/app2.css'
-].forEach(require)
-
-//webpack无法使用node模块, 这里了require是webpack自己了require, 不是node的require
-/*const fs = require('fs')
-fs.readDir('./', (file) => {
-    console.log(file)
-})*/
-
 const angular = require('angular')
-const eden = angular.module('eden', [])
+require('materialize-css')
+require('materialize-css/dist/css/materialize.css')
 
-//引入controller
-;[
-    './controller/HomeController',
-    './controller/TestController'
-].forEach((controller) => {
-    require(controller)(eden)
+const app = angular.module('eden', [])
+app.controller('TestController', function($scope) {
+	$scope.hello = '你要去海兰'
 })
--- node/webpack.config.js
+++ node/webpack.config.js
@@ -1,26 +1,34 @@
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
 
 module.exports = {
     entry: {
         index: __dirname + '/src/app.js'
     },
     output: {
-        path: __dirname + '/public/dist',
+        path: __dirname + '/dist/js',
         filename: '[name].js'
     },
     plugins: [
         commonsPlugin,
         new HtmlWebpackPlugin({
-            filename: 'index.html',
+            filename: '../index.html',
             template: 'src/index.html'
-        })
+        }),
+        new ExtractTextPlugin('[name].bundle.css')
     ],
     module: {
         loaders: [
-            { test: /\.css$/, loader: 'style-loader!common-loader' },
-            { test: /\.html$/, loader: 'html' }
+            { test: /\.css$/, loader: 'style!css' },
+            { test: /\.html$/, loader: 'html' },
+            { test: require.resolve('materialize-css'), loader: 'exports?Materialize' },
+            {
+                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+                loader: 'url-loader?limit=8192&name=[path][name].[ext]'
+            },
+            //{ test: /\.js$/, loader: 'babel' },
         ]
     },
     resolve: {
