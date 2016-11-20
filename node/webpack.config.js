fa19f0cd9a5792b2d88092f205b08dd49ff0f657
Deltax9k <deltax9000@163.com>
2016-11-20 19:23:58 +0800

    new webpack config

--- node/webpack.config.js
+++ node/webpack.config.js
@@ -8,34 +8,36 @@ module.exports = {
         index: __dirname + '/src/app.js'
     },
     output: {
-        path: __dirname + '/dist/js',
-        filename: '[name].js'
+        path: __dirname + '/dist/',
+        filename: '[name].bundle.js',
     },
     plugins: [
         commonsPlugin,
         new HtmlWebpackPlugin({
-            filename: '../index.html',
+            filename: __dirname + '/dist/index.html',
             template: 'src/index.html'
         }),
         new ExtractTextPlugin('[name].bundle.css')
     ],
     module: {
-        loaders: [
-            { test: /\.css$/, loader: 'style!css' },
-            { test: /\.html$/, loader: 'html' },
-            { test: require.resolve('materialize-css'), loader: 'exports?Materialize' },
-            {
-                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
-                loader: 'url?limit=8192&name=js/[name].[ext]'
-            },
-            { test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015']
-                }
-            }
-        ]
+        loaders: [{
+            test: /\.css$/,
+            loader: 'style!css'
+        }, {
+            test: /\.html$/,
+            loader: 'html'
+        }, {
+            test: require.resolve('materialize-css'),
+            loader: 'exports?Materialize'
+        }, {
+            test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+            loader: 'url?limit=8192&name=[ext]/[name].[ext]'
+        }, {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel',
+            query: { presets: ['es2015'] }
+        }]
     },
     resolve: {
     }
